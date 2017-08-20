d95199a3c001fc7794053369951704d497ee0e77
unknown <csaivyas@example.com>
2017-08-19 19:28:20 -0700

    yes
    Signed-off-by: unknown <csaivyas@example.com>

---
+++ Initials.java
@@ -0,0 +1,22 @@
+package ibg;
+
+public class Initials {
+	/*main method is the entry point to any java program which is static so no need to create any object
+	to main method and its public which can be called from anywhere and it doesn't return anything so return type is void*/
+	public static void main(String[] args) {
+		//printing initials on to console using '*' character
+		System.out.println();
+        System.out.println("      *               *     ***********");
+        System.out.println("       *             *     *            ");
+        System.out.println("        *           *      *            ");
+        System.out.println("         *         *       *               ");
+        System.out.println("          *       *        *              ");
+        System.out.println("           *     *         *           ");
+        System.out.println("            *   *          *               ");
+        System.out.println("             * *           *               ");
+        System.out.println("              *             ***********");
+        System.out.println();
+     }  // end main()
+	}
+
+
